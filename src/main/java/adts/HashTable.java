2a1829eac0d2cbe48892dc641d58889b5f09f661
Kenny Ballou <kb@devnulllabs.io>
2024-02-12 11:47:45 -0700

    hashtables: without chains
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- src/main/java/adts/HashTable.java
+++ src/main/java/adts/HashTable.java
@@ -34,6 +34,46 @@ public class HashTable implements ADT {
         }
     }
 
+    private final int size;
+    private KV[] table;
+
+    public HashTable() {
+        this(10);
+    }
+
+    public HashTable(int size) {
+        this.size = size;
+        this.table = new KV[size];
+    }
+
+    private int hash(String key) {
+        return key.hashCode() % this.size;
+    }
+
+    public void add(String key, int value) {
+        int k = hash(key);
+        table[k] = new KV(key, value);
+    }
+
+    public Optional<Integer> get(String key) {
+        int k = hash(key);
+        return Optional.ofNullable(table[k])
+            .map(kv -> kv.value);
+    }
+
+    public Optional<Integer> remove(String key) {
+        int k = hash(key);
+        Optional<Integer> value = Optional.ofNullable(table[k])
+            .map(kv -> kv.value);
+        table[k] = null;
+        return value;
+    }
+
+    public boolean contains(String key) {
+        int k = hash(key);
+        return table[k] != null;
+    }
+
     @Override
     public String toString() {
         return Stream.of(this.table)
